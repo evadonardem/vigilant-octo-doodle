d272df3cbd372a57589e34609495c826fb232969
NAME <mail@gmail.com>
2022-08-11 00:49:03 +0800

    Added options to select report type by store, category, or location. Added summary per item section.

--- resources/js/components/ReportsSalesInvoiceMonitoring.js
+++ resources/js/components/ReportsSalesInvoiceMonitoring.js
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
 import { Breadcrumb, Button, ButtonGroup, Card, Form, Jumbotron } from 'react-bootstrap';
 import cookie from 'react-cookies';
+import CommonDropdownSelectSingleStore from './CommonDropdownSelectSingleStore';
 import CommonDropdownSelectSingleStoreCategory from './CommonDropdownSelectSingleStoreCategory';
+import CommonDropdownSelectSingleStoreLocation from './CommonDropdownSelectSingleStoreLocation';
 
 const END_POINT = `${apiBaseUrl}/reports/sales-invoices-monitoring`;
 const DT_SALES_INVOICES_MONITORING = `table-sales-invoices-monitoring`;
 const DT_SALES_INVOICES_MONITORING_INVOICES = `table-sales-invoices-monitoring-invoices`;
+const DT_SALES_INVOICES_MONITORING_SUMMARY = `table-sales-invoices-monitoring-summary`;
 
 export default class ReportsSalesInvoiceMonitoring extends Component {
     constructor(props) {
         super(props);
+        this.handleChangeType = this.handleChangeType.bind(this);
         this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
-        this.handleStoreCategoryChange = this.handleStoreCategoryChange.bind(this);
+        this.handleGenerateCsvReport = this.handleGenerateCsvReport.bind(this);
 
         this.state = {
-            token: null,
-            searchFilters: null,
             booklets: [],
-            selectedCategory: {},
+            csvFilters: null,
+            reportType: 'store',
+            searchFilters: null,
+            summary: [],
+            token: null,
         };
     }
 
     componentDidMount() {
         const self = this;
         const token = cookie.load('token');
-        const { booklets } = self.state;
+        const { booklets, summary } = self.state;
         const currencyFormat = $.fn.dataTable.render.number(',', '.', 2, 'Php').display;
 
         self.setState({
@@ -220,7 +226,77 @@ export default class ReportsSalesInvoiceMonitoring extends Component {
                 }
             }
         });
+        
+        $(`.${DT_SALES_INVOICES_MONITORING_SUMMARY}`).DataTable({
+            data: summary,
+            buttons: [],
+            columns: [
+                { data: 'code' },
+                { data: 'name' },
+                {
+					data: 'quantity_original',
+					render: $.fn.dataTable.render.number(','),
+				},
+                {
+					data: 'quantity_actual',
+					render: $.fn.dataTable.render.number(','),
+				},
+                {
+					data: 'quantity_bad_orders',
+					render: $.fn.dataTable.render.number(','),
+				},
+                {
+					data: 'quantity_returns',
+					render: $.fn.dataTable.render.number(','),
+				},
+            ],
+            columnDefs: [
+                {
+                    targets: [2, 3, 4, 5],
+                    className: 'dt-right',
+                },
+            ],
+            footerCallback: function (row, data, start, end, display) {
+                const api = this.api();
+
+                const intVal = function ( i ) {
+                    return typeof i === 'string'
+                        ? i.replace(/[\$,]/g, '')*1
+                        : typeof i === 'number' ? i : 0;
+                };
+                
+                const offset = 2;
+				for (let i = 0; i < 4; i++) {
+					const totalAmount = api
+						.column(offset + i)
+						.data()
+						.reduce(
+							function (a, b) {
+								return intVal(a) + intVal(b);
+							},
+							0
+						);                    
+                    $(api.column(offset + i).footer()).html(currencyFormat(totalAmount));
+				}
+            },
+            ordering: false,
+            paging: false,
+            searching: false,
+        });
     }
+    
+    handleChangeType(e) {
+		const self = this;
+		const reportType = e.target.value;
+		self.setState({
+			...self.state,
+			booklets: [],
+			csvFilters: null,
+			searchFilters: null,
+			summary: [],
+			reportType,
+		});
+	}
 
     handleSearchSubmit(e) {
         e.preventDefault();
@@ -229,34 +305,56 @@ export default class ReportsSalesInvoiceMonitoring extends Component {
         const table = $('.data-table-wrapper')
             .find(`table.${DT_SALES_INVOICES_MONITORING}`)
             .DataTable();
+        const tableSummary = $('.data-table-wrapper')
+            .find(`table.${DT_SALES_INVOICES_MONITORING_SUMMARY}`)
+            .DataTable();
         const data = $(e.currentTarget).serialize();
+        const csvFilters = data;
 
         axios.get(`${END_POINT}?${data}&token=${token}`)
             .then((response) => {
                 const { data: booklets, meta } = response.data;
-                const { search_filters: searchFilters } = meta;
+                const { search_filters: searchFilters, summary } = meta;
                 self.setState({
                     ...self.state,
                     searchFilters,
-                    booklets
+                    booklets,
+                    summary,
+                    csvFilters,
                 });
                 table.clear();
                 table.rows.add(booklets).draw();
+                tableSummary.clear();
+                tableSummry.rows.add(summary).draw();
             });
     }
-
-    handleStoreCategoryChange(e) {
-        const self = this;
-        self.setState({
-            ...self.state,
-            selectedCategory: e
-        })
-    }
+    
+    handleGenerateCsvReport(e) {
+		e.preventDefault();
+		const reportType = e.target.getAttribute('data-report-type');
+		const self = this;
+		const { csvFilters, token } = self.state;
+		axios.get(`${END_POINT}?${csvFilters}&generate=csv&report_type=${reportType}&token=${token}`, {
+			responseType: 'arraybuffer',
+		})
+		.then(response => {
+			const filename = response.headers['content-disposition'].split('filename=')[1].split('.')[0];
+			const extension = response.headers['content-disposition'].split('.')[1].split(';')[0];
+			const blob = new Blob(
+				[response.data],
+				{ type: 'text/csv' }
+			);
+			const link = document.createElement('a');
+			link.href = window.URL.createObjectURL(blob);
+			link.download = `${filename}.${extension}`;
+			link.click();
+		});
+	}
 
     render() {
         const {
+			reportType,
             searchFilters,
-            selectedCategory,
         } = this.state;
 
         return (
@@ -281,11 +379,36 @@ export default class ReportsSalesInvoiceMonitoring extends Component {
                                         <Form.Label>To:</Form.Label>
                                         <Form.Control type="date" name="to"/>
                                     </Form.Group>
-                                    <CommonDropdownSelectSingleStoreCategory
-                                        label="Sold To:"
-                                        name="category_id"
-                                        handleChange={this.handleStoreCategoryChange}
-                                        selectedCategory={selectedCategory}/>
+                                    <Form.Group>
+                                        <Form.Label>By:</Form.Label>
+                                        <Form.Check
+											type="radio"
+											name="by"
+											label="Store"
+											value="store"
+											checked={reportType === 'store'}
+											onClick={this.handleChangeType}/>
+										<Form.Check
+											type="radio"
+											name="by"
+											label="Category"
+											value="category"
+											checked={reportType === 'category'}
+											onClick={this.handleChangeType}/>
+                                        <Form.Check
+											type="radio"
+											name="by"
+											label="Location"
+											value="location"
+											checked={reportType === 'location'}
+											onClick={this.handleChangeType}/>
+                                    </Form.Group>
+                                    { reportType === 'store' && 
+										<CommonDropdownSelectSingleStore name="store_id"/> }
+									{ reportType === 'category' && 
+										<CommonDropdownSelectSingleStoreCategory name="category_id"/> }
+									{ reportType === 'location' && 
+										<CommonDropdownSelectSingleStoreLocation name="location_id"/> }
                                     <hr className="my-4"/>
                                     <Button type="submit" block>Generate Report</Button>
                                 </Form>
@@ -303,29 +426,80 @@ export default class ReportsSalesInvoiceMonitoring extends Component {
                             }
                             <Card.Body>
                                 <div style={!searchFilters ? {display: 'none'} : null}>
-                                    <table className={`table table-striped ${DT_SALES_INVOICES_MONITORING}`} style={{width: 100+'%'}}>
-                                        <thead>
-                                            <tr>
-                                                <th scope="col"></th>
-                                                <th scope="col">Booklet No.</th>
-                                                <th scope="col">Total Sales</th>
-                                                <th scope="col">Less VAT</th>
-                                                <th scope="col">Amount Net less VAT</th>
-                                                <th scope="col">Total Amount Due</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody></tbody>
-                                        <tfoot>
-                                            <tr>
-                                                <th></th>
-                                                <th>Total:</th>
-                                                <th>0.00</th>
-                                                <th>0.00</th>
-                                                <th>0.00</th>
-                                                <th>0.00</th>
-                                            </tr>
-                                        </tfoot>
-                                    </table>
+									<Card>
+										<Card.Header>
+											<b>Full Detail</b> (Per Booklet)
+										</Card.Header>
+										<Card.Body>
+											<div className="row">
+												<div className="col-md-12">
+													<Button className="pull-right" onClick={this.handleGenerateCsvReport} data-report-type="full">
+														<i className="fa fa-icon fa-download"></i> CSV
+													</Button>
+												</div>
+											</div>
+											<table className={`table table-striped ${DT_SALES_INVOICES_MONITORING}`} style={{width: 100+'%'}}>
+												<thead>
+													<tr>
+														<th scope="col"></th>
+														<th scope="col">Booklet No.</th>
+														<th scope="col">Total Sales</th>
+														<th scope="col">Less VAT</th>
+														<th scope="col">Amount Net less VAT</th>
+														<th scope="col">Total Amount Due</th>
+													</tr>
+												</thead>
+												<tbody></tbody>
+												<tfoot>
+													<tr>
+														<th></th>
+														<th>Total:</th>
+														<th>0.00</th>
+														<th>0.00</th>
+														<th>0.00</th>
+														<th>0.00</th>
+													</tr>
+												</tfoot>
+											</table>
+										</Card.Body>
+									</Card>
+                                    <Card className="mt-4">
+										<Card.Header>
+											<b>Summary</b> (Per Item)
+										</Card.Header>
+										<Card.Body>
+											<div className="row">
+												<div className="col-md-12">
+													<Button className="pull-right" onClick={this.handleGenerateCsvReport} data-report-type="summary">
+														<i className="fa fa-icon fa-download"></i> CSV
+													</Button>
+												</div>
+											</div>
+											<table className={`table table-striped ${DT_SALES_INVOICES_MONITORING_SUMMARY}`} style={{width: 100+'%'}}>
+												<thead>
+													<tr>
+														<th scope="col">Code</th>
+														<th scope="col">Name</th>
+														<th scope="col">Total Sales</th>
+														<th scope="col">Less VAT</th>
+														<th scope="col">Amount Net less VAT</th>
+														<th scope="col">Total Amount Due</th>
+													</tr>
+												</thead>
+												<tbody></tbody>
+												<tfoot>
+													<tr>
+														<th scope="col"></th>
+														<th scope="col" style={{textAlign: 'right'}}>Total:</th>
+														<th scope="col"></th>
+														<th scope="col"></th>
+														<th scope="col"></th>
+														<th scope="col"></th>
+													</tr>
+												</tfoot>
+											</table>
+										</Card.Body>
+									</Card>
                                 </div>
                                 { !searchFilters &&
                                     <Jumbotron className="mb-0">
